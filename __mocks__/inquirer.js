a32ccdf716586a0ecda020e09482ba1a0417acb4
wanthering <kend888@163.com>
2019-04-11 21:09:50 +0800

    完整弹窗功能

--- __mocks__/inquirer.js
+++ __mocks__/inquirer.js
@@ -6,41 +6,91 @@ exports.expectPrompts = assertions => {
   pendingAssertions = assertions
 }
 
+
 exports.prompt = async prompts => {
-  // 验证是否存在缓存参数。
   if (!pendingAssertions) {
-    throw new Error(`inquirer正在被Mock，且缺少缓存参数: ${prompts}`)
+    throw new Error(`inquirer was mocked and used without pending assertions: ${prompts}`)
   }
 
   const answers = {}
-
-  // 打印一下prompts和pendingAssertions
+  let skipped = 0
   prompts.forEach((prompt, i) => {
-    const a = pendingAssertions[i]
-
-    // 验证message弹窗和预期一样
-    if(a.message){
-      expect(prompt.message).toMatch(a.message)
+    if (prompt.when && !prompt.when(answers)) {
+      skipped++
+      return
     }
 
-    // 验证choices是数组，弹窗和预期值一样
-    if(a.choices){
-      expect(prompt.choices.length).toBe(a.choices.length)
-      for(let i in a.choices){
-        expect(prompt.choices[i].name).toMatch(a.choices[i])
+    const setValue = val => {
+      if (prompt.validate) {
+        const res = prompt.validate(val)
+        if (res !== true) {
+          throw new Error(`validation failed for prompt: ${prompt}`)
+        }
       }
+      answers[prompt.name] = prompt.filter
+        ? prompt.filter(val)
+        : val
     }
 
-    if(a.choose != null){
-      // choose有值的情况下，type一般为list
-      expect(prompt.type).toBe('list')
+    const a = pendingAssertions[i - skipped]
+    if (!a) {
+      console.error(`no matching assertion for prompt:`, prompt)
+      console.log(prompts)
+      console.log(pendingAssertions)
+    }
 
-      // 设置answers成为与正版相同结果
-      answers[prompt.name] = prompt.choices[a.choose].value
+    if (a.message) {
+      const message = typeof prompt.message === 'function'
+        ? prompt.message(answers)
+        : prompt.message
+      expect(message).toMatch(a.message)
+    }
+
+    const choices = typeof prompt.choices === 'function'
+      ? prompt.choices(answers)
+      : prompt.choices
+    if (a.choices) {
+      expect(choices.length).toBe(a.choices.length)
+      a.choices.forEach((c, i) => {
+        const expected = a.choices[i]
+        if (expected) {
+          expect(choices[i].name).toMatch(expected)
+        }
+      })
+    }
+
+    if (a.input != null) {
+      expect(prompt.type).toBe('input')
+      setValue(a.input)
+    }
+
+    if (a.choose != null) {
+      expect(prompt.type === 'list' || prompt.type === 'rawList').toBe(true)
+      setValue(choices[a.choose].value)
+    }
+
+    if (a.check != null) {
+      expect(prompt.type).toBe('checkbox')
+      setValue(a.check.map(i => choices[i].value))
+    }
+
+    if (a.confirm != null) {
+      expect(prompt.type).toBe('confirm')
+      setValue(a.confirm)
+    }
+
+    if (a.useDefault) {
+      expect('default' in prompt).toBe(true)
+      setValue(
+        typeof prompt.default === 'function'
+          ? prompt.default(answers)
+          : prompt.default
+      )
     }
   })
 
-  expect(prompts.length).toBe(pendingAssertions.length)
+  expect(prompts.length).toBe(pendingAssertions.length + skipped)
+  pendingAssertions = null
 
   return answers
 }
